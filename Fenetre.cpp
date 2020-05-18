7ec7611fa89cca443e32a0799444d7af13a45fa5
pellegrin.pierre <pierre.pellegrin@esqese.ucly.fr>
2020-05-19 00:04:47 +0200

    Variable qui contient les coordonnées de la case cliqué

--- Fenetre.cpp
+++ Fenetre.cpp
@@ -20,13 +20,22 @@ void Fenetre::run() {
             }
             if (sf::Mouse::isButtonPressed(sf::Mouse::Left)) {
                 sf::Vector2i position = sf::Mouse::getPosition(*this);
+
+                float caseCliqueX = grille.convertirGrilleEnPixels(
+                        grille.convertirPixelsEnGrille(position.x, position.y).x,
+                        grille.convertirPixelsEnGrille(position.x, position.y).y).x;
+                float caseCliqueY = grille.convertirGrilleEnPixels(
+                        grille.convertirPixelsEnGrille(position.x, position.y).x,
+                        grille.convertirPixelsEnGrille(position.x, position.y).y).y;
+                sf::Vector2f caseClique(caseCliqueX, caseCliqueY);
+
                 std::cout << "Coordonnees : "
                           << grille.convertirGrilleEnPixels(grille.convertirPixelsEnGrille(position.x, position.y).x,
                                                             grille.convertirPixelsEnGrille(position.x, position.y).y).x
                           << " / "
                           << grille.convertirGrilleEnPixels(grille.convertirPixelsEnGrille(position.x, position.y).x,
                                                             grille.convertirPixelsEnGrille(position.x, position.y).y).y
-                          << " Cases : " << grille.convertirPixelsEnGrille(position.x, position.y).x << "/"
+                          << " Cases : " << grille.convertirPixelsEnGrille(position.x, position.y).x << " / "
                           << grille.convertirPixelsEnGrille(position.x, position.y).y << " : (" << position.x << " , "
                           << position.y << ")" << std::endl;
             }

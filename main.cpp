6a5561061f2c0ec323fd6fb16773f669c26027a0
pellegrin.pierre <pierre.pellegrin@esqese.ucly.fr>
2020-06-06 19:29:12 +0200

    Nouveau test de logique en remplaçant la boucle par des valurs qui s'incrémentent

--- main.cpp
+++ main.cpp
@@ -2,9 +2,13 @@
 #include <SFML/Graphics.hpp>
 #include "Fenetre.h"
 #include "Grille.h"
+#include "Logique.h"
 
 int main() {
-    Fenetre fenetre(800, 800);
-    fenetre.run();
+    Logique oui;
+    oui.recupGrilleSolution();
+    //Fenetre fenetre(800, 800);
+    //fenetre.run();
+
     return 0;
 }

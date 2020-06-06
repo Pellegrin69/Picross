09ed827c71cb45069524c4c4a63bd42f986d7d8e
pellegrin.pierre <pierre.pellegrin@esqese.ucly.fr>
2020-06-06 19:15:30 +0200

    Premier test de Logique

--- Logique.cpp
+++ Logique.cpp
@@ -3,3 +3,50 @@
 //
 
 #include "Logique.h"
+#include "Grille.h"
+#include <fstream>
+
+
+Logique::Logique() {
+    //calcul_bombes = 0
+    //while calcul_bombes < nb_bombes:
+    //x = int(random.randint(0, nb_cases_horizontales - 1))
+    //y = int(random.randint(0, nb_cases_verticales - 1))
+    //if m_grille[y][x] == 0:
+    //m_grille[y][x] = 9
+    //calcul_bombes += 1
+    //comptage_bombes(m_grille)
+
+}
+
+void Logique::recupGrilleSolution() {
+    std::string const cheminFichier("C:/Users/pellegrin.pierre/CLionProjects/Picross/solutionCarre.txt");
+    std::ifstream monFlux(cheminFichier.c_str());
+    int grille[10][10];
+    if (monFlux) {
+        char carac;
+        while (monFlux.get(carac)) {
+            for (int j = 0; j < 10; ++j) {
+                for (int i = 0; i < 10; ++i) {
+                    if (carac == '0') {
+                        grille[j][i] = 0;
+                        std::cout << "zero" << std::endl;
+                    } else if (carac == '1') {
+                        grille[j][i] = 1;
+                        std::cout << "un" << std::endl;
+                    } else if (carac == '2') {
+                        grille[j][i] = 2;
+                        std::cout << "deux" << std::endl;
+                    }
+                }
+            }
+        }
+    } else {
+        std::cout << "Une erreur s'est produite : impossible d'ouvrir le fichier" << std::endl;
+    }
+    for (int j = 0; j < 10; ++j) {
+        for (int i = 0; i < 10; ++i) {
+            std::cout << grille[j][i] << std::endl;
+        }
+    }
+}
